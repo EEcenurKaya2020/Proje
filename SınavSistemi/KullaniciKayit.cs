6500bc3737e1b63fc323cbd7d6cc436a760d1ef7
EEcenurKaya2020 <74544465+EEcenurKaya2020@users.noreply.github.com>
2022-04-26 16:37:17 +0300

    Proje dosyası ekle.

---
+++ SınavSistemi/KullaniciKayit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SınavSistemi
+{
+    public partial class KullaniciKayit : Form
+    {
+        public KullaniciKayit()
+        {
+            InitializeComponent();
+        }
+        static string constring = "Data Source=DESKTOP-HVTB9LK;Initial Catalog=YazilimYapimi;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(constring);
+        string g;
+
+        private void KullaniciKayit_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+        }
+    }
+}
